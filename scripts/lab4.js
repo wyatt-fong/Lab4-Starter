3c899f2a6e3c4cc309533accde5c30c89896778a
Wyatt Fong <fongwyatt@gmail.com>
2025-04-29 21:09:57 -0700

    Fixed small initial bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
