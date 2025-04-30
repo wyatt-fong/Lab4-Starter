89273532fd34cca9628e32d4998266d7b64ebd0a
Wyatt Fong <fongwyatt@gmail.com>
2025-04-29 21:13:21 -0700

    Fixed more errors in discountedPrice

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -25,11 +25,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length === 0) {
+        return false;
+    }  
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
